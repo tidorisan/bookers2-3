f5f46fb8a9a33e0680ab22864b01de3ef4f29391
vagrant <vagrant@localhost.localdomain>
2020-07-05 01:39:42 +0000

    [crate]user/index/show/edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  # def new
-  # end
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   get 'users/new'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # homes controller
   root  "homes#top"
   get "homes/about" => "homes#about"
+  resources :users, only: [:index]
 end

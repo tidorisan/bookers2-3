aabd7ddcc349982956ac14cd65b5b1dcc2d94670
vagrant <vagrant@localhost.localdomain>
2020-07-05 02:05:32 +0000

    [update]uers/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-  def index
-  end
+	def index
+		@book = Book.new
+	end
 
-  def show
-  end
+	def show
+	end
 
-  def edit
-  end
+	def edit
+
+	end
 end
